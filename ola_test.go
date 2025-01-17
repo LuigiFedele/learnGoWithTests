d85a9aea95f2a0fb57d1e58072a893a5d574bce8
Luigi Fedele <luigiffedele@outlook.com>
2025-01-17 12:50:00 -0300

    Refatoração do código

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Cris")
-	esperado := "Olá, Cris"
-
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("Diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Cris")
+		esperado := "Olá, Cris"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("Diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
 }
